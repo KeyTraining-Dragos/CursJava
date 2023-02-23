3761564f94527733e1b645e7f744a51169dd112c
Dragos Tanta <dragos.tanta@gmail.com>
2023-02-23 19:50:30 +0200

    first commit

---
+++ src/curs1/FirstClass.java
@@ -0,0 +1,22 @@
+package curs1;
+
+//--one line comment
+
+/*
+ * multi line
+ * comment
+ */
+
+public class FirstClass {
+	
+	//metoda main- metoda principala de executie	
+	public static void main(String[] args) {
+
+		System.out.println("Test");
+		
+	}
+	
+	
+	
+
+}
