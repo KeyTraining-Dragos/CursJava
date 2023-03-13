d49e6d1134b7879ff38a7818a9138439bea2fd17
Dragos Tanta <dragos.tanta@gmail.com>
2023-03-13 19:48:34 +0200

    method override example

---
+++ src/curs7/Deer.java
@@ -0,0 +1,15 @@
+package curs7;
+
+public class Deer extends Animal {
+
+	
+	@Override
+	public void makeSound() {	
+		System.out.println("Meee !!!");
+	}
+	
+	public void eatGrass() {
+		System.out.println("I like eating grass!");
+	}
+	
+}
